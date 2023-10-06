8e56021e3ebcccc98ea9508fb79c24a3f23c2992
IbraheemCodes <>
2023-10-06 22:23:03 +0000

    styled the graphics and interactions aswell as started the javascript by adding parameters and variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
-console.log ("testing!!")
+class Player {
+    constructor(id, name, score, streak) {
+        this.id = id;
+        this.name = name;
+        this.score = score;
+        this.streak = streak;
+    }
+
+    getScore() {
+        return this.score;
+    }
+
+    getStreak() {
+        return this.streak;
+    }
+
+    incrementScore() {
+        this.score++;
+    }
+
+    incrementStreak() {
+        this.streak++;
+    }
+
+    resetStreak() {
+        this.streak = 0;
+    }
+}
